feac3899d159125c386c350184f811b03da7640f
korLethean <korlethean@gmail.com>
2024-07-17 15:04:24 +0900

    minor fix

--- NCC-Sign/reference_implementation/test/test_speed.c
+++ NCC-Sign/reference_implementation/test/test_speed.c
@@ -6,7 +6,7 @@
 #include "speed_print.h"
 #include "stdio.h"
 
-#define NTESTS 1
+#define NTESTS 10000
 
 uint64_t t[NTESTS];
 
@@ -29,60 +29,61 @@ printf("shake assembly mode \n");
 
   t_overhead=19;
 
-  // for(i = 0 ; i < NTESTS ; i++)
-  // {
-  //   t[i] = cpucycles();
-  //   poly_uniform_eta(&a, seed, 0);
-  // }
-  // print_results("poly_uniform_eta:", t, NTESTS);
+  for(i = 0 ; i < NTESTS ; i++)
+  {
+    t[i] = cpucycles();
+    poly_uniform_eta(&a, seed, 0);
+  }
+  print_results("poly_uniform_eta:", t, NTESTS);
 
-  // for(i = 0 ; i < NTESTS ; i++)
-  // {
-  //   t[i] = cpucycles();
-  //   poly_uniform_gamma1(&a, seed, 0);
-  // }
-  // print_results("poly_uniform_gamma1:", t, NTESTS);
+  for(i = 0 ; i < NTESTS ; i++)
+  {
+    t[i] = cpucycles();
+    poly_uniform_gamma1(&a, seed, 0);
+  }
+  print_results("poly_uniform_gamma1:", t, NTESTS);
 
-  // for(i = 0 ; i < NTESTS ; i++)
-  // {
-  //   t[i] = cpucycles();
-  //   ntt(a.coeffs, b.coeffs);
-  // }
-  // print_results("poly_ntt:", t, NTESTS);
+  for(i = 0 ; i < NTESTS ; i++)
+  {
+    t[i] = cpucycles();
+    ntt(a.coeffs, b.coeffs);
+  }
+  print_results("poly_ntt:", t, NTESTS);
 
-  // for(i = 0 ; i < NTESTS ; i++)
-  // {
-  //   t[i] = cpucycles();
-  //   invntt_tomont(a.coeffs, b.coeffs);
-  // }
-  // print_results("poly_invntt:", t, NTESTS);
+  for(i = 0 ; i < NTESTS ; i++)
+  {
+    t[i] = cpucycles();
+    invntt_tomont(a.coeffs, b.coeffs);
+  }
+  print_results("poly_invntt:", t, NTESTS);
 
-  // for(i = 0 ; i < NTESTS ; i++)
-  // {
-  //   t[i] = cpucycles();
-  //   poly_challenge(&a, seed);
-  // }
-  // print_results("poly_challenge:", t, NTESTS);
+  for(i = 0 ; i < NTESTS ; i++)
+  {
+    t[i] = cpucycles();
+    poly_challenge(&a, seed);
+  }
+  print_results("poly_challenge:", t, NTESTS);
 
   for(i = 0 ; i < NTESTS ; i++)
   {
     t[i] = cpucycles();
     crypto_sign_keypair(pk, sk);
   }
+  print_results("Keypair:", t, NTESTS);
 
-  // for(i = 0 ; i < NTESTS ; i++)
-  // {
-  //   t[i] = cpucycles();
-  //   crypto_sign(sm, &smlen, sm, CRHBYTES, sk);
-  // }
-  // print_results("Sign:", t, NTESTS);
+  for(i = 0 ; i < NTESTS ; i++)
+  {
+    t[i] = cpucycles();
+    crypto_sign(sm, &smlen, sm, CRHBYTES, sk);
+  }
+  print_results("Sign:", t, NTESTS);
 
-  // for(i = 0 ; i < NTESTS ; i++)
-  // {
-  //   t[i] = cpucycles();
-  //   crypto_sign_verify(sm, CRYPTO_BYTES, sm, CRHBYTES, pk);
-  // }
-  // print_results("Verify:", t, NTESTS);
+  for(i = 0 ; i < NTESTS ; i++)
+  {
+    t[i] = cpucycles();
+    crypto_sign_verify(sm, CRYPTO_BYTES, sm, CRHBYTES, pk);
+  }
+  print_results("Verify:", t, NTESTS);
 
   return 0;
 }
